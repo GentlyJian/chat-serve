b8de40b77b29d13e4c49bd212c0b666146f0964d
zjianzzhang <zjianzzhang@tencent.com>
2023-03-23 17:09:08 +0800

    chat post

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import Koa from "koa"
 import Router from "koa-router";
 import {ChatGPTAPI} from 'chatgpt'
+import bodyParser from "koa-bodyparser";
 
 
 
@@ -8,23 +9,29 @@ import {ChatGPTAPI} from 'chatgpt'
 
 
 const app = new Koa()
+app.use(bodyParser())
 const router = new Router();
 const chatApi = new ChatGPTAPI({
   apiKey: process.env.OPENAI_API_KEY || 'sk-',
+  completionParams: {
+    model:'gpt-3.5-turbo',
+    top_p: 0.8
+  }
 });
 
-router.get("/chat", async (ctx, next) => {
+router.post("/chat", async (ctx, next) => {
     // 获取请求中的参数
-    const { message } = ctx.request.query;
+    const { message,parentMessageId } = ctx.request.body;
     console.log(message) 
+    const pamras = parentMessageId ? [message, { parentMessageId, timeoutMs: 60 * 1000}] : [message,{timeoutMs: 60 * 1000}]
   try {
-    const ret =await chatApi.sendMessage(message);
+    const ret =await chatApi.sendMessage(...pamras);
     const {id,parentMessageId, text} = ret
     // 将生成的内容返回给客户端
-    ctx.body = {id,parentMessageId, text}
+    ctx.body = {id, parentMessageId, text, code:0}
   }catch(err) {
     console.log(err)
-    ctx.body = {message: 'error'}
+    ctx.body = {message: 'error',code:-1}
   }
 });
 
@@ -32,6 +39,6 @@ router.get("/chat", async (ctx, next) => {
 app.use(router.routes()).use(router.allowedMethods());
 
 // 启动服务器
-app.listen(process.env.PORT || '3000', () => {
-    console.log("Server is listening on port " + (process.env.PORT || '3000'));
+app.listen(process.env.PORT || '3001', () => {
+    console.log("Server is listening on port " + (process.env.PORT || '3001'));
 });
